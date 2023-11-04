390d92d9178a5333466f5201879e7cf76e1ae942
GabrielAlberini <77715611+GabrielAlberini@users.noreply.github.com>
2023-11-04 01:03:22 -0300

    Vista para petición equivocada

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,14 +1,15 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { List } from "../components/List/List";
 import { Users } from "../components/Users/Users";
+import { NotFound } from "../components/NotFound/NotFound";
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Users />} />
-        <Route path="/:slug" element={<List />} />
-        <Route path="*" element="hola"></Route>
+        <Route path="/user/:slug" element={<List />} />
+        <Route path="*" element={<NotFound />}></Route>
       </Routes>
     </BrowserRouter>
   );
